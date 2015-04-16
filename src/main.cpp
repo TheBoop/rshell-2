f8eb78eac554e190b0dd214dedc6323f9ce94ffe
Leon Zhu <lzhu012@hammer.cs.ucr.edu>
2015-04-16 00:28:43 -0700

    added basic execvp bash calls

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
+#include <string>
 #include <string.h>
-#include <unistd.h> //execvp
+#include <unistd.h> //execvp, fork, gethostname, getlogin
 #include <stdio.h> //perror
 #include <errno.h> //perror
-#include <vector>
 #include <sys/types.h> //wait
 #include <sys/wait.h> //wait
 using namespace std;
@@ -31,11 +31,32 @@ int main()
 		//strcpy(input_cstr, input.c_str());
 		token = strtok(in_cstr, delim); // &save_1);
 		//cout << "token: " << token << endl;
+		//char* path;
+		char* argv[1024];
+		int i = 0;
 		cout << "Tokens of input:" << endl;
 		while(token != NULL)
 		{
 			cout << token << endl;
+			argv[i] = token;
+			i++;
 			token = strtok(NULL, delim);
+		}
+		int pid = fork();
+		if(pid == -1)
+		{
+			perror("fork");
+		}
+		else if(pid != 0)
+		{
+			cout << "In parent, going to wait" << endl;
+			wait(0);
+		}
+		else if(pid == 0)
+		{
+			cout << "In child" << endl;
+			execvp(argv[0], argv);
+
 		}
 		//vector<string> words; //not sure if i need to do this
 	}
