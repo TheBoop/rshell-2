df6408620a4bd1f3e31462e448e0c775284503c6
Leon Zhu <lzhu012@hammer.cs.ucr.edu>
2015-04-16 17:22:03 -0700

    fixed ls bug

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <string.h>
+#include <stdlib.h>
 #include <unistd.h> //execvp, fork, gethostname, getlogin
 #include <stdio.h> //perror
 #include <errno.h> //perror
@@ -13,7 +14,11 @@ int main()
 	string input;	
 	while(1)
 	{
-		cout << "$ ";
+		char* login = getlogin();
+		//char name[64];
+		//int len = 64;
+		//int hostname = gethostname(); // how to use this??
+		cout << login << "@"  << "$ ";
 		getline(cin, input);
 		if(input == "exit")
 		{
@@ -23,16 +28,26 @@ int main()
 		char* in_cstr = new char[input.length()+1];
 		//char* in_cstr[input.length()+1]; doesn't work
 		strcpy(in_cstr, input.c_str());
-		
+		//char in_cstr[100];
+		//cin.getline(in_cstr, 100, '\n');
+
+		//int find = 0;
+		//int track = 0;
+		//find = in_cstr.find("&&");
+		//if(find > 0)
+		//{
+		//	track = 1;
+		//}
+
 		//tokenize words
-		char delim[] = " ";
+		char delim[] = "  &&";
 		char* token;
 		token = strtok(in_cstr, delim); // &save_1);
 		//cout << "token: " << token << endl;
-		//char* path;
+		//char* file;
 		//cout << "c_string: " << token[0] << endl; testing
 		//cout << "c_string: " << token[1] << endl; testing
-		char* argv[1024];
+		char** argv = new char*[input.length()+1];
 		int i = 0;
 		cout << "Tokens of input:" << endl;
 		//int x = strlen(token);
@@ -42,6 +57,8 @@ int main()
 			//cout << "k:" << k;
 			//cout << token[k] << endl;
 			//token = strtok(NULL, delim); //causes problems with loop
+			//x = strlen(token);
+			//k=0;
 		//}
 		while(token != NULL)
 		{
@@ -50,28 +67,36 @@ int main()
 			i++;
 			token = strtok(NULL, delim);
 		}
+		argv[i] = NULL; //fixes ls -a ls bug gotta put null char back
 		int pid = fork();
 		if(pid == -1)
 		{
 			perror("fork failed");
+			exit(1);
 		}
-		else if(pid != 0)
+		else if(pid > 0)
 		{
 			cout << "In parent, going to wait" << endl;
-			wait(0);
+			if(wait(0) == -1)
+			{
+				perror("wait failed");
+			}
 		}
 		else if(pid == 0)
 		{
 			cout << "In child" << endl;
-			if(execvp(argv[0], argv) != 0)
+
+			if(execvp(argv[0], argv) == -1)
 			{
 				perror("execvp failed");
 			}
-			else
-			{
-				execvp(argv[0], argv);
-			}
+			//else
+			//{
+			//	execvp(argv[0], argv);
+			//}
 		}
+		delete[] in_cstr;
+		delete[] argv;
 	}
 	//currently if ls -a run then ls, ls -a still runs
 	//will give me login (e.g. lzhu012)
