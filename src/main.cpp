870092bb7df3979827adc5a8f7199fd11d40630c
Leon Zhu <lzhu012@hammer.cs.ucr.edu>
2015-04-18 03:53:49 -0700

    connectors sort of work better

--- src/main.cpp
+++ src/main.cpp
@@ -83,37 +83,29 @@ int main()
 		char* save_1;
 		char* save_2;
 		token = strtok_r(in_cstr, delim, &save_1);
-		//cout << "token: " << token << endl;
-		//char* file;
-		//cout << "c_string: " << token[0] << endl; testing
-		//cout << "c_string: " << token[1] << endl; testing
-		//
-		//char** argv = new char*[input.length()+1];
-		//int i = 0;
-		//cout << "Tokens of input:" << endl;
-		//
-		//int x = strlen(token);
-		//cout << "x:" << x;
-		//for(int k = 0; k < x; k++)
-		//{
-			//cout << "k:" << k;
-			//cout << token[k] << endl;
-			//token = strtok(NULL, delim); //causes problems with loop
-			//x = strlen(token);
-			//k=0;
-		//}
-
+		
 		while(token != NULL)
 		{
+			//cout << "Top of lopp" << endl;
+			//cout << "toke: " << token << endl;
 			char* in_str2 = new char[strlen(token)+1];
 			in_str2 = token;
 			token2 = strtok_r(in_str2, " ", &save_2);
+			//cout << "in_str2: " << in_str2 << endl;
+			//cout << "token2: " << token2 << endl;
+
 			vector<string> temp;
 			while(token2 != NULL)
 			{
 				temp.push_back(token2);
 				token2 = strtok_r(NULL, " ", &save_2);
 			}
+			//cout << "contents of temp vector: ";
+			//for(unsigned t = 0; t < temp.size(); t++)
+			//{
+			//	cout << temp[t] << " ";
+			//}
+			//cout << endl;
 
 			char **argv = new char*[temp.size()+1];
 			for(unsigned i = 0; i < temp.size(); i++)
@@ -124,8 +116,15 @@ int main()
 				}
 				argv[i] = new char[temp.at(i).size()+1];
 				strcpy(argv[i], temp.at(i).c_str());
-				argv1[temp.size()] = 0;
+				argv[temp.size()] = NULL;
 			}
+
+			//cout << "contents of argv: ";
+			//for(unsigned k = 0; k < temp.size(); k++)
+			//{
+			//	cout << argv[k] << " ";
+			//}
+			//cout << endl;
 
 			int pid = fork();
 			int status = 0;
@@ -136,87 +135,60 @@ int main()
 			}
 			else if(pid == 0)
 			{
-				if(execvp(argv[0], argv) != -1 && or_cnt == 1)
-				{
-					status = 10;
-					exit(1);
-				}
-				else if(execvp(argv[0], argv) == -1)
+				//cout << "in child" << endl;
+				if(execvp(argv[0], argv) == -1)
 				{
 					perror("execvp failed");
+					if(or_cnt == 1)
+					{
+						status = 5;
+						exit(1);
+					}
 					if(and_cnt == 1)
 					{
-						status = 10;
+						status = 5;
 						exit(1);
 					}
-					else if(or_cnt == 1)
+				}
+				else
+				{
+					if(or_cnt == 1)
 					{
-						cout << "or failed" << endl;
-						status = 1;
-						cout << status << endl;
-						exit(1);
+						status = 5;
+						break;
 					}
-					exit(1);
 				}
 			}
 			else
 			{
+				//cout << "in parent" << endl;
 				if(wait(&status) == -1)
 				{
 					perror("wait failed");
 				}
 				else
 				{
-					if(status > 0 && and_cnt == 1)
+					if(WEXITSTATUS(status) != 0 && and_cnt == 1)
 					{
 						break;
 					}
 					if(WEXITSTATUS(status) == 0 && or_cnt == 1)
 					{
-						cout << status << " or worked " << endl;
 						break;
 					}
 					else
 					{
+						//if wait is sucessful, and no and is found no or is found
 						token = strtok_r(NULL, delim, &save_1);
 					}
 				}
 			}
 		}
-		//delete[] in_cstr;
+		delete[] in_cstr;
 		//delete[] in_str2;
 	}
+	return 0;
 }
 //nohlsearch
 //bug: if execvp fails, exit has to be ran twice to work for some reason
 //bug: &&& doesnt give error
-//IDEA
-//STEP 1:
-//LETS SAY WE HAVE     ls && ls
-//Tokenize with delimintors && ; ||
-//THEN WE HAVE TOKENS
-//     ls 
-// ls
-// STORE the first in one argv1
-// then second in another argv2
-// recurssviely do this until no delims are left
-// then tokenize AGAIN with " "
-// so for first argv1 we have ls
-// the second also ls
-// then we execvp firs the first argv
-// then we call execvp vp on the second but this time dependent on the first execvp
-// do this until all argvs are passed through
-// how to code this i have no idea, will it work i have no idea
-//
-// char* token;
-// token = strtok(in_cstr, "&")
-// token[0] = '    ls '
-// token[1] = ' ls'
-// each one of these tokens will become its own cstring
-// token1 = strtok(token, " ")
-// if(execvp(token1) == -1)
-// 	perrror("failed")
-// 	exit
-// else(execvp(token2) == -1)
-// recurrively call (token2, token1)
-// 
