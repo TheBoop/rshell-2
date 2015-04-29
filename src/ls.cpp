cfac938f31b7d8f82bd648fc93c0825c23d323cc
Leon Zhu <lzhu012@hammer.cs.ucr.edu>
2015-04-29 07:39:08 -0700

    added flag checking

--- src/ls.cpp
+++ src/ls.cpp
@@ -13,8 +13,81 @@
 
 using namespace std;
 
+/*
+void paramcheck(int argc, char* argv[])
+{
+	vector<string> param;
+	vector<string> arg;
+	//int a_param = 0;
+	//int l_param = 0;
+	//int R_param = 0;
+	for(int i = 1; i < argc; i++)
+	{
+		param.push_back(argv[i]);
+	}
+	for(unsigned i = 1; i < param.size(); i++)
+	{
+		if(param.at(i).at(0) == '-')
+		{
+			arg.push_back(param.at(i));
+		}
+	}
+	for(unsigned i = 0; i < param.size(); i++)
+	{
+		cout << "i:" << i << " " << param.at(i) << endl;
+	}
+	for(unsigned i = 0; i < arg.size(); i++)
+	{
+		if(arg.at(i).find('l') != string::npos)
+		{
+			cout << "found l" << endl;
+		}
+	}
+}
+*/
+
+void check(char argv[], vector<int> flags)
+{
+	int a_param = 0;
+	int l_param = 0;
+	int R_param = 0;
+	if(argv[0] == '-')
+	{
+		for(int i = 1; argv[i] != '\0'; i++)
+		{
+			if(argv[i] == 'a')
+			{
+				a_param = 1;
+			}
+			if(argv[i] == 'l')
+			{
+				l_param = 1;
+			}
+			if(argv[i] == 'R')
+			{
+				R_param = 1;
+			}
+		}
+		flags.push_back(a_param);
+		flags.push_back(l_param);
+		flags.push_back(R_param);
+	}
+	//display flags check
+	for(unsigned j = 0; j < flags.size(); j++)
+	{
+		cout << "j:" << j << " "  << flags.at(j) << endl;
+	}
+}
+
+
 int main(int argc, char* argv[])
 {
+	//paramcheck(argc, argv);
+	vector<int> flags;
+	for(int i = 1; i < argc; i++)
+	{
+		check(argv[i], flags);
+	}
 	const char *dir = ".";
 	DIR *dirp;
 	if(NULL == (dirp = opendir(dir)))
